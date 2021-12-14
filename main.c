1fba388565ae5560ce6d3db37b33fefad55fd5fd
Sonya <74297783+sonyamonyaa@users.noreply.github.com>
2021-12-14 22:45:23 +0200

    setup

---
+++ main.c
@@ -0,0 +1,25 @@
+#define TXT 1024 //256?
+#define WORD 30
+// #define UP_A 00 
+// #define UP_Z 00
+// #define LOW_A 00
+// #define LOW_Z 00
+
+/*
+atbash char
+
+if( char >= 'a' && char <= 'z')
+	new char = z - char mod a
+if( char >= 'A' && char <= 'Z')
+	new char = Z - char mod A
+return new char
+*/
+
+/*
+gematria val
+
+if( char >= 'a' && char <= 'z')
+	val = char - a + 1
+if( char >= 'A' && char <= 'Z')
+	val = char - A + 1
+*/
