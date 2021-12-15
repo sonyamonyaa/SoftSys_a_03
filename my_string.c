d752cd348c15d3ce461ab80aba607d37090d2717
Israel Jacobovitz <learsi0545488592@gmail.com>
2021-12-15 13:28:17 +0200

    strct

---
+++ my_string.c
@@ -0,0 +1,24 @@
+int gimatricEquals(char* s1, char* s2){
+
+}
+
+int abatashEquals(char* s1, char* s2){
+    if( char >= 'a' && char <= 'z'){
+        new char = z - char mod a;
+    }
+    if( char >= 'A' && char <= 'Z'){
+        new char = Z - char mod A;
+    }
+
+    return new char
+}
+
+int anagramEquals(char* s1, char* s2){
+    if( char >= 'a' && char <= 'z'){
+        val = char - a + 1;
+    }
+
+    if( char >= 'A' && char <= 'Z'){
+        val = char - A + 1;
+    }
+}
