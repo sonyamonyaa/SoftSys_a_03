c3b111d97a311b2a545117e44f994f72dd0292af
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2021-12-20 20:07:29 +0200

    main

--- main.c
+++ main.c
@@ -6,34 +6,19 @@
 int main() {
     char text[TXT];
     char word[WORD];
-    // calloc() allocate the memory and set 0 to all of them
-    // text = (char *)calloc(TXT, sizeof(char));
-    // word = (char *)calloc(WORD, sizeof(char));
+
     //input
     inputWord(word);
     inputTxt(text);
-    //print inputs
-    // puts(word);
-    // puts(text);
-    printf("\n");
-    printf("%s\n", word);
-    printf("%s", text);
-    printf("\n");
 
     //program stuff
+    printGematric(text);
+    printf("\n");
+
 //    printf("Atbash Sequences: ");
 //    printAtbashEquals(text, word);
-
-    printAnagramEquals(text, word);
     printf("\n");
-//    works
-//    printGematric(text);
-//    printf("\n");
+    printAnagramEquals(text, word);
 
-
-
-    //free the memory at the end of the program
-    // free(text);
-    // free(word);
     return 0;
 }
