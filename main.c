6fd4c32343a03c3b61cc0d7ca1100d61c0c0514f
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2021-12-20 20:07:29 +0200

    main

--- main.c
+++ main.c
@@ -1,39 +1,22 @@
 #include "stringLib.h"
 #include <stdio.h>
-#include <string.h>
-//#include <stdlib.h>
 
 int main() {
     char text[TXT];
     char word[WORD];
-    // calloc() allocate the memory and set 0 to all of them
-    // text = (char *)calloc(TXT, sizeof(char));
-    // word = (char *)calloc(WORD, sizeof(char));
+
     //input
     inputWord(word);
     inputTxt(text);
-    //print inputs
-    // puts(word);
-    // puts(text);
-    printf("\n");
-    printf("%s\n", word);
-    printf("%s", text);
-    printf("\n");
 
     //program stuff
+    printGematric(text);
+    printf("\n");
+
 //    printf("Atbash Sequences: ");
 //    printAtbashEquals(text, word);
-
-    printAnagramEquals(text, word);
     printf("\n");
-//    works
-//    printGematric(text);
-//    printf("\n");
+    printAnagramEquals(text, word);
 
-
-
-    //free the memory at the end of the program
-    // free(text);
-    // free(word);
     return 0;
 }
